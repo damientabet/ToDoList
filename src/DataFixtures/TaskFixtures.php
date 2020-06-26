2b5d32c49a4e1ab15884e6a6301b477b8b901594
TABET Damien <tabetdamien@free.fr>
2020-06-26 17:20:15 +0200

    Create data fixtures

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $task = new Task();
+        $task->setTitle('Ma tâche');
+        $task->setContent('Ma première tâche');
+        $task->setCreatedAt(new \DateTime());
+        $task->setUser($this->getReference('user'));
+
+        $manager->persist($task);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class
+        );
+    }
+}
